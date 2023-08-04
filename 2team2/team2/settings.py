27372b849cd954e0762b7ccaa5501e67b9180abc
leejiwon <2jiwone@ewhain.net>
2023-08-04 14:16:31 +0900

    feat:수정 사항

--- 2team2/team2/settings.py
+++ 2team2/team2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'service',
 ]
 
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL='service.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
