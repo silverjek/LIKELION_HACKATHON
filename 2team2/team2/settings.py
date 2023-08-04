ad90f78de8a933f8839b6002b07a647f375b0795
leejiwon <2jiwone@ewhain.net>
2023-08-04 14:46:11 +0900

    test:2

--- 2team2/team2/settings.py
+++ 2team2/team2/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL='service.User'
+#AUTH_USER_MODEL='service.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -109,7 +109,7 @@ AUTH_USER_MODEL='service.User'
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Seoul'
-#
+# ##
 
 USE_I18N = True
 
