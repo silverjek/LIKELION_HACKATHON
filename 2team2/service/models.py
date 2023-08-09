53e0bf57e193cc0e8dac1495949eaedca9e01899
silverjek <lovelyjieun2@gmail.com>
2023-08-09 22:01:14 +0900

    feat:수정1

--- 2team2/service/models.py
+++ 2team2/service/models.py
@@ -5,10 +5,17 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class User(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20) #기본 8자 이상의 규칙이 있어 minimum 설정은 건너뛰었습니다
     userType = models.BooleanField(default=True) #True-일반사용자 / False-의료인사용자
-    latestUpdate = models.DateTimeField(null=True, blank=True)
+    
 
+    #userName --- 아이디, 비밀번호, 이름은 기본 필드가 존재
+    #userSSN --- 이 아래로 User 모델의 정보를 실사용하는 경우가 없음
+    #userPhone
+    #userEmail
+    #userAddress
+    #userPatient
 
 class Medi_Info(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -36,7 +43,8 @@ class Medi_Info(models.Model):
     patHeight = models.FloatField()
     patWeight = models.FloatField()
     patPhone = PhoneNumberField(unique=True, null=False, blank=False) #unique 설정을 풀어야할지도.. 의료인의 경우 환자로 또 가입하려면 한 번호로 두번 가입해야하니까
-    updateDate = models.DateTimeField(auto_now_add=True)
+    updateDate = models.DateTimeField(auto_now_add=True) #의료기록 자체 갱신날짜
+    latestUpdate = models.DateTimeField(null=True, blank=True)
     #approveDate = models.DateTimeField() --- erd에는 있는데 api명세서에는 없어서 일단 빼두었습니다
 
 class Caution(models.Model):
